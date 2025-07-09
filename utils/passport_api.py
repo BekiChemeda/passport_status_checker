3c7e1fe352c64faae2a924c7239a1f6a83f47999
Beknan Chemeda <122085319+BekiChemeda@users.noreply.github.com>
2025-07-09 12:31:15 +0300

    Update passport_api.py

--- utils/passport_api.py
+++ utils/passport_api.py
@@ -19,7 +19,7 @@ def check_by_reference(reference, lang="en"):
         ref_value = reference.strip()
         json_data = {"Tracking_Code": ref_value}
         if len(ref_value) < 7:
-            return "❌ Tracking Code must be at least 7 characters long \."
+            return "❌ Tracking Code must be at least 7 characters long \\."
         res = requests.post(
             "https://passport.ics.gov.et/track",
             json=json_data,
@@ -30,14 +30,14 @@ def check_by_reference(reference, lang="en"):
         # print("Response from /track:", data)
 
         if not isinstance(data, list) or len(data) == 0:
-            return ("❌ Your passport is either not ready or the input is invalid \. \n"
-                    "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\! እባክዎን በትዕግስት ይጠብቁ\!")
+            return ("❌ Your passport is either not ready or the input is invalid \\. \n"
+                    "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\\! እባክዎን በትዕግስት ይጠብቁ\\!")
 
         messages = []
         for d in data:
             messages.append(
                 f"✅ **እንኳን ደስ አለዎት\!**\n\n"
-            f"ፓስፖርትዎ ስለደረስ **{d['brancham']}** ለእርስዎ በተፈቀደልዎ መቀበያ ቀናት ብቻ በመቅረብ መውሰድ ይችላሉ\!\n\n"
+            f"ፓስፖርትዎ ስለደረስ **{d['brancham']}** ለእርስዎ በተፈቀደልዎ መቀበያ ቀናት ብቻ በመቅረብ መውሰድ ይችላሉ\\!\n\n"
             
             f"📄 **Tracking Code:**\n``` {d['ref_code']}```\n"
             f"👤 **ሙሉ ስም / Full Name:**\n``` {d['name']} {d['fathers_name']} {d['grand_fathers_name']}```\n"
@@ -49,14 +49,14 @@ def check_by_reference(reference, lang="en"):
 
     except Exception as e:
         print(f"Error in check_by_reference: {e}")
-        return ("❌ Your passport is either not ready or the input is invalid \. \n"
-                "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\! እባክዎን በትዕግስት ይጠብቁ\!")
+        return ("❌ Your passport is either not ready or the input is invalid \\. \n"
+                "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\\! እባክዎን በትዕግስት ይጠብቁ\\!")
 
 
 def check_by_fullname(full_name, branch, lang="en"):
     try:
         if len(full_name.split(" ")) < 3:
-            return "❌ Please enter your full name including your Grandfather's Name \."
+            return "❌ Please enter your full name including your Grandfather's Name \\."
         encoded_name = quote(full_name)
         encoded_branch = quote(branch)
 
@@ -66,8 +66,8 @@ def check_by_fullname(full_name, branch, lang="en"):
         json_resp = res.json()
 
         if not json_resp.get("ok"):
-            return ("❌ Your passport is either not ready or the input is invalid \. \n"
-                    "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\! እባክዎን በትዕግስት ይጠብቁ\!")
+            return ("❌ Your passport is either not ready or the input is invalid \\. \n"
+                    "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\\! እባክዎን በትዕግስት ይጠብቁ\\!")
 
         d = json_resp["data"]
         return (
@@ -82,8 +82,8 @@ def check_by_fullname(full_name, branch, lang="en"):
         )
     except Exception as e:
         print(f"Error in check_by_fullname: {e}")
-        return ("❌ Your passport is either not ready or the input is invalid \. \n"
-                "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\! እባክዎን በትዕግስት ይጠብቁ\!")
+        return ("❌ Your passport is either not ready or the input is invalid \\. \n"
+                "⏱ ይቅርታ ፓስፖርትዎ አልደረሰም\\! እባክዎን በትዕግስት ይጠብቁ\\!")
 
 
 # print(check_by_fullname("ABEBE KEBEDE HAILE", "ICS Saris Adey Abeba Branch", "en"))
