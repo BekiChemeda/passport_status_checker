3b7be09e9250b0dcf3709ef79873f356a05da69a
Beknan Chemeda <122085319+BekiChemeda@users.noreply.github.com>
2025-08-05 01:22:42 +0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,8 +1,13 @@
-# utils/database.py
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads variables from .env file into environment
 
 MONGO_URI = os.getenv("MONGO_URI")
+if MONGO_URI is None:
+    raise Exception("MONGO_URI is not set")
+
 client = MongoClient(MONGO_URI)
 db = client["passport_bot"]
 
